290770a2d4c77c304a0142c5fd8cd8de94ad0bb1
Jeff <jz@pocketflock.com>
2013-09-07 14:08:59 -0400

    constants

---
+++ Llamas/src/com/PennApps/llamas/Constants.java
@@ -0,0 +1,14 @@
+package com.PennApps.llamas;
+
+
+public class Constants {
+	public final int slope_0o = 0;
+	public final int slope_15 = 1;
+	public final int slope_30 = 2;
+	public final int slope_45 = 3;
+	public final int trampoline = 4;
+	public final int death_gap = 999;
+	public final int death_spike = 998;
+	
+	
+}
--- Llamas/src/com/PennApps/llamas/MainActivity.java
+++ Llamas/src/com/PennApps/llamas/MainActivity.java
@@ -12,14 +12,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
 
 	// Gesture listener class
 	
